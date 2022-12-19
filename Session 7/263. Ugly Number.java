beae5bcc3200059e3e05cc8123af633e72197064
Kevin Shin <118714261+kevinthebear@users.noreply.github.com>
2022-12-19 20:50:52 +0900

    Update 263. Ugly Number.java

--- Session 7/263. Ugly Number.java
+++ Session 7/263. Ugly Number.java
@@ -3,25 +3,16 @@ class Solution {
         if (n == 0) {
             return false;
         }
-        while (n % 2 == 0 || n % 3 == 0 || n % 5 == 0) {
-            if (n % 2 == 0) {
-                n = n/2;
-            }
-            else if (n % 3 == 0) {
-                n = n/3;
-            }
-            else if (n % 5 == 0) {
-                n = n/5;
-            }
-            else {
-                break;
-            }
-        }       
-        if (n == 1) {
-            return true;
+        while (n % 2 == 0) {
+            n = n/2;
         }
-        else {
-            return false;
+        while (n % 3 == 0) {
+            n = n/3;
         }
+        while (n % 5 == 0) {
+            n = n/5;
+        }
+        if (n == 1) return true;
+        else return false;
     }
 }
