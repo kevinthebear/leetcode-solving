60eb42ff63cafa3881aff2010f12ee365a5a32a1
Kevin Shin <118714261+kevinthebear@users.noreply.github.com>
2022-12-19 19:15:57 +0900

    Create 217. Contains Duplicate.java

---
+++ Session 7/217. Contains Duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!hm.containsKey(nums[i])) {
+                hm.put(nums[i], i);
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
